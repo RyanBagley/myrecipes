eb100667b45e9da864cd4d260c67778e3c4ca10b
Ryan Bagley <ryan.bagley99@gmail.com>
2018-04-24 13:30:23 +0100

    Layouts with test

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,5 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update, :delete]
 
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 5)
@@ -11,6 +12,7 @@ class ChefsController < ApplicationController
   def create
     @chef = Chef.new(chef_params)
     if @chef.save
+      session[:chef_id] = @chef.id
       flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
       redirect_to chef_path(@chef)
     else
@@ -20,16 +22,13 @@ class ChefsController < ApplicationController
 
 
   def show
-    @chef = Chef.find(params[:id])
     @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
-    @chef = Chef.find(params[:id])
   end
 
   def update
-    @chef = Chef.find(params[:id])
     if @chef.update(chef_params)
       flash[:success] = "Your account was updated successfully!"
       redirect_to @chef
@@ -39,7 +38,6 @@ class ChefsController < ApplicationController
   end
 
   def destroy
-    @chef = Chef.find(params[:id])
     @chef.destroy
     flash[:danger] = "Chef and all associate recipes have been deleted!"
     redirect_to chefs_path
@@ -49,6 +47,10 @@ class ChefsController < ApplicationController
 
   def chef_params
     params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+  end
+
+  def set_chef
+    @chef = Chef.find(params[:id])
   end
 
 end
